1d5bbe16a42f7b1f5e10eccfc4e07efede97d245
manemejeff <kmb14056@yandex.ru>
2022-04-12 23:10:12 +0400

    react elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const el = <h1>Todo App</h1>;
+const el = (
+  <div>
+    <h1>Todo App</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
 
 ReactDOM.render(el, document.getElementById("root"));
