0eb4b27c2cf559e2758aec933984e3b3f273e35a
manemejeff <kmb14056@yandex.ru>
2022-04-17 21:34:49 +0400

    arrays as props

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -2,17 +2,16 @@ import React from "react";
 
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
-  return (
-    <ul>
+const TodoList = ({ todos }) => {
+  const elements = todos.map((item) => {
+    return (
+      // eslint-disable-next-line react/jsx-key
       <li>
-        <TodoListItem label="Drink Cofee" />
+        <TodoListItem {...item} />
       </li>
-      <li>
-        <TodoListItem label="Build React App" important />
-      </li>
-    </ul>
-  );
+    );
+  });
+  return <ul>{elements}</ul>;
 };
 
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+  const todoData = [
+    { label: "Drink Cofee", important: false },
+    { label: "Make Awesome App", important: true },
+    { label: "Get a Job", important: false },
+  ];
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData} />
     </div>
   );
 };
 
+// ReactDOM.render(App, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 // eslint-disable-next-line jest/require-hook
-ReactDOM.render(App, document.getElementById("root"));
+root.render(<App />);
