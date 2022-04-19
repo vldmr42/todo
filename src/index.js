4e998c2271e96f0a5d7bc4130061f7e32d3362be
manemejeff <kmb14056@yandex.ru>
2022-04-19 21:26:59 +0400

    import css

--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ import { createRoot } from "react-dom/client";
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
+
+import "./index.css";
 
 const App = () => {
   const todoData = [
-    { label: "Drink Cofee", important: false, id: 1 },
+    { label: "Drink Coffee", important: false, id: 1 },
     { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Get a Job", important: false, id: 3 },
+    { label: "Have a lunch", important: false, id: 3 },
   ];
+
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
       <TodoList todos={todoData} />
     </div>
   );
