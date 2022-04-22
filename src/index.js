3aea8d2332f02d4ce3ccfcdad9fd3ef47034e49f
manemejeff <kmb14056@yandex.ru>
2022-04-22 14:06:20 +0400

    Proj structure p2

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+import {
+  createRoot
+} from "react-dom/client";
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
-
-import "./index.css";
-
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 },
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+import App from './components/app'
 
 // ReactDOM.render(App, document.getElementById("root"));
 const container = document.getElementById("root");
 const root = createRoot(container);
 // eslint-disable-next-line jest/require-hook
-root.render(<App />);
+root.render( < App /> );
